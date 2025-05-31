88db0420b03b899a299d7a3b57b3c2b52758c56a
buiTannn <22119228@student.hcmute.edu.vn>
2025-05-31 23:44:30 +0700

    Visual Studio

---
+++ Memory_Management/Heap 2/Heap 2/main.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+// Task thực hiện cấp phát và giải phóng bộ nhớ
+void memoryManagementTask(void* pvParameters) {
+    void* ptr1, * ptr2, * ptr3, * ptr4;
+
+    printf("Before allocation: Free heap = %u bytes\n", (unsigned int)xPortGetFreeHeapSize());
+
+    // Cấp phát bộ nhớ
+    ptr1 = pvPortMalloc(100); // Cấp 100B
+    printf("After allocating 100B: Free heap = %u bytes\n", (unsigned int)xPortGetFreeHeapSize());
+
+    ptr2 = pvPortMalloc(50); // Cấp 50B
+    printf("After allocating 50B: Free heap = %u bytes\n", (unsigned int)xPortGetFreeHeapSize());
+
+    ptr3 = pvPortMalloc(100); // Cấp 100B
+    printf("After allocating 100B: Free heap = %u bytes\n", (unsigned int)xPortGetFreeHeapSize());
+
+    // Giải phóng bộ nhớ
+    vPortFree(ptr3);
+    printf("After freeing 100B: Free heap = %u bytes\n", (unsigned int)xPortGetFreeHeapSize());
+
+    //ptr4 = pvPortMalloc(140);
+    //ptr4 = pvPortMalloc(100); 
+    ptr4 = pvPortMalloc(80); 
+    if (ptr4 == NULL) {
+        printf("Failed to allocate 80B for ptr4\n");
+    }
+    else {
+        printf("After allocating 80B at %p: Free heap = %u bytes\n", ptr4, (unsigned int)xPortGetFreeHeapSize());
+    }
+
+}
+
+int main(void) {
+    xTaskCreate(memoryManagementTask, "MemoryTask", 1024, NULL, 1, NULL);
+
+    // Bắt đầu scheduler
+    vTaskStartScheduler();
+    for (;;) {}
+}
---
+++ Memory_Management/Heap 5/Heap 5/main.c
@@ -0,0 +1,54 @@
+﻿#include "FreeRTOS.h"
+#include "portable.h"
+#include <stdio.h>
+
+/* Khai báo 2 mảng để làm heap regions */
+static uint8_t ucHeap1[16384];  /* 16KB */
+static uint8_t ucHeap2[32768];  /* 32KB */
+
+/* Định nghĩa 2 vùng nhớ cho heap */
+const HeapRegion_t xHeapRegions[] =
+{
+    { ucHeap1, sizeof(ucHeap1) },  // Vùng 1: 16KB
+    { ucHeap2, sizeof(ucHeap2) },  // Vùng 2: 32KB
+    { NULL, 0 }                    
+};
+
+/* Hiển thị thông tin heap */
+void vShowHeapInfo(void)
+{
+    printf("Free heap: %zu bytes\n", xPortGetFreeHeapSize());
+}
+
+int main(void)
+{
+    /* Khởi tạo heap_5 */
+    vPortDefineHeapRegions(xHeapRegions);
+    printf("Vung 1: %p, 16KB\n", ucHeap1);
+    printf("Vung 2: %p, 32KB\n", ucHeap2);
+
+    vShowHeapInfo();
+
+    /* Cấp phát bộ nhớ */
+    void* ptr1 = pvPortMalloc(1024);
+    void* ptr2 = pvPortMalloc(2048);
+
+    if (ptr1 && ptr2) {
+        printf("Cap phat thanh cong: 3072 bytes\n");
+    }
+    else {
+        printf("Error\n");
+    }
+
+    vShowHeapInfo();
+
+    /* Giải phóng bộ nhớ */
+    vPortFree(ptr1);
+    vPortFree(ptr2);
+    printf("Free ptr 1 va ptr 2\n");
+
+    vShowHeapInfo();
+
+    printf("Demo hoàn thành!\n");
+    return 0;
+}
